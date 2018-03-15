bccff4c7ef6dee5e8f6feac98ed7cb883b20a106
Hegde Pashupati <hegdep1@cs-256.org.aalto.fi>
2018-03-15 16:12:10 +0200

    gpflow compatible toy example

--- onoffgpf/utils.py
+++
@@ -1,6 +0,0 @@
-import time
-def printtime(start):
-    end = time.time()
-    hours, rem = divmod(end - start, 3600)
-    minutes, seconds = divmod(rem, 60)
-    return str("{:0>2}:{:0>2}:{:05.2f}".format(int(hours), int(minutes), seconds))
